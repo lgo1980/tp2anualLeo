a2665e67352b3f4f037d9189b1425af69b7bb4f3
Leonardo <lgo80@yahoo.com.ar>
2025-06-15 00:35:09 -0300

    algunas mejoras

---
+++ src/main/java/ar/edu/utn/dds/k3003/controller/ColeccionController.java
@@ -0,0 +1,36 @@
+package ar.edu.utn.dds.k3003.controller;
+
+import ar.edu.utn.dds.k3003.facades.FachadaFuente;
+import ar.edu.utn.dds.k3003.facades.dtos.ColeccionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/colecciones")
+public class ColeccionController {
+
+  private final FachadaFuente fachadaFuente;
+
+  @Autowired
+  public ColeccionController(FachadaFuente fachadaFuente) {
+    this.fachadaFuente = fachadaFuente;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<ColeccionDTO>> listarColecciones() {
+    return ResponseEntity.ok(fachadaFuente.colecciones());
+  }
+
+  @GetMapping("/{nombre}")
+  public ResponseEntity<ColeccionDTO> obtenerColeccion(@PathVariable String nombre) {
+    return ResponseEntity.ok(fachadaFuente.buscarColeccionXId(nombre));
+  }
+
+  @PostMapping
+  public ResponseEntity<ColeccionDTO> crearColeccion(@RequestBody ColeccionDTO coleccion) {
+    return ResponseEntity.ok(fachadaFuente.agregar(coleccion));
+  }
+} 
