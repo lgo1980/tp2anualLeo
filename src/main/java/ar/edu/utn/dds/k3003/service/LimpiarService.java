905f5afc8e9efaf35828e32772f387fb5300e234
Leonardo <lgo80@yahoo.com.ar>
2025-09-11 20:38:18 -0300

    Arreglos de fachada 15

--- src/main/java/ar/edu/utn/dds/k3003/service/LimpiarService.java
+++ src/main/java/ar/edu/utn/dds/k3003/service/LimpiarService.java
@@ -1,33 +1,46 @@
 package ar.edu.utn.dds.k3003.service;
 
+import ar.edu.utn.dds.k3003.model.Agregador;
+import ar.edu.utn.dds.k3003.repository.AgregadorRepository;
+import ar.edu.utn.dds.k3003.repository.AgregadorRepositoryImpl;
 import ar.edu.utn.dds.k3003.repository.ColeccionRepositoryImpl;
 import ar.edu.utn.dds.k3003.repository.ConsensoRepositoryImpl;
 import ar.edu.utn.dds.k3003.repository.FuenteRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 public class LimpiarService {
 
   private final ConsensoRepositoryImpl consensoRepository;
   private final FuenteRepositoryImpl fuenteRepositoryImpl;
-  private final ColeccionRepositoryImpl coleccionRepositoryImpl;
+  private final AgregadorRepositoryImpl agregadorRepositoryImpl;
 
   @Autowired
   public LimpiarService(FuenteRepositoryImpl fuenteRepositoryImpl,
                         ConsensoRepositoryImpl consensoRepository,
-                        ColeccionRepositoryImpl coleccionRepositoryImpl) {
+                        AgregadorRepositoryImpl agregadorRepositoryImpl) {
     this.consensoRepository = consensoRepository;
     this.fuenteRepositoryImpl = fuenteRepositoryImpl;
-    this.coleccionRepositoryImpl = coleccionRepositoryImpl;
+    this.agregadorRepositoryImpl = agregadorRepositoryImpl;
   }
 
   @Transactional
   public void limpiarEntidades() {
+    List<Agregador> agregadores = (List<Agregador>) agregadorRepositoryImpl.findAll();
+    for (Agregador a : agregadores) {
+      if (a.getConsensos() != null) {
+        a.getConsensos().clear(); // limpia el Map en memoria
+        agregadorRepositoryImpl.save(a); // persiste los cambios
+      }
+    }
+
+    // 2️⃣ Borrar todas las tablas de datos dependientes
     consensoRepository.deleteAll();
     fuenteRepositoryImpl.deleteAll();
-    coleccionRepositoryImpl.deleteAll();
+    agregadorRepositoryImpl.deleteAll();
   }
 
 }
