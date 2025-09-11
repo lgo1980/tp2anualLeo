806aef9777d5625f92a55974a1340dcaecd3f742
Leonardo <lgo80@yahoo.com.ar>
2025-09-11 19:48:07 -0300

    Arreglos de fachada 13

--- src/main/java/ar/edu/utn/dds/k3003/service/LimpiarService.java
+++ src/main/java/ar/edu/utn/dds/k3003/service/LimpiarService.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.dds.k3003.service;
 
+import ar.edu.utn.dds.k3003.repository.ColeccionRepositoryImpl;
 import ar.edu.utn.dds.k3003.repository.ConsensoRepositoryImpl;
 import ar.edu.utn.dds.k3003.repository.FuenteRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +12,22 @@ public class LimpiarService {
 
   private final ConsensoRepositoryImpl consensoRepository;
   private final FuenteRepositoryImpl fuenteRepositoryImpl;
+  private final ColeccionRepositoryImpl coleccionRepositoryImpl;
 
   @Autowired
   public LimpiarService(FuenteRepositoryImpl fuenteRepositoryImpl,
-                 ConsensoRepositoryImpl consensoRepository) {
+                        ConsensoRepositoryImpl consensoRepository,
+                        ColeccionRepositoryImpl coleccionRepositoryImpl) {
     this.consensoRepository = consensoRepository;
     this.fuenteRepositoryImpl = fuenteRepositoryImpl;
+    this.coleccionRepositoryImpl = coleccionRepositoryImpl;
   }
 
   @Transactional
   public void limpiarEntidades() {
     consensoRepository.deleteAll();
     fuenteRepositoryImpl.deleteAll();
+
   }
 
 }
